9f05f66337a7a7c321f7630b5fb45fd27251d9e3
Zade Hart <zadehartpro@gmail.com>
2025-01-27 19:50:37 -0700

    Updated server.ts in server folder.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import path from 'node:path';
-import type { Request, Response } from 'express';
+// import path from 'node:path';
+// import type { Request, Response } from 'express';
 import db from './config/connection.js'
 import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
 import { expressMiddleware } from '@apollo/server/express4';
@@ -29,10 +29,10 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    app.use(express.static('../client/dist'));
+  
+     app.get('*', (_req, res) => {
+      res.sendFile('../client/dist/index.html');
     });
   }
 
@@ -43,3 +43,5 @@ const startApolloServer = async () => {
 };
 
 startApolloServer();
+
+
